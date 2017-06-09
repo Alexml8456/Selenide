9db5fa334c0b8cbc6337ad7010992f72cb015373
amelnychuk <Alexander.Melnychuk@keystonett.com>
2017-06-09 18:09:49 +0300

    Added some of CFD Rollover tests

--- src/test/java/backoffice/RunSuiteTest.java
+++ src/test/java/backoffice/RunSuiteTest.java
@@ -3,8 +3,7 @@ package backoffice;
 import backoffice.about.AboutScenarios;
 import backoffice.login.SuccessLogin;
 import backoffice.login.UnSuccessLogin;
-import backoffice.rollover.schedulers.RolloverSchedulerScenarios;
-import backoffice.rollover.schedulers.EditSchedulerScenarios;
+import backoffice.rollover.schedulers.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -15,7 +14,11 @@ import org.junit.runners.Suite;
         UnSuccessLogin.class,
         AboutScenarios.class,
         RolloverSchedulerScenarios.class,
-        EditSchedulerScenarios.class
+        EditSchedulerScenarios.class,
+        CloneSchedulerScenarios.class,
+        SchedulerValidationScenarios.class,
+        SimulateValidationScenarios.class,
+        SimulateSchedulerScenarios.class
 })
 
 public class RunSuiteTest {
